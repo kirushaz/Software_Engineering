00280d0da387507716db0457d8045725f194cb24
kirushaz <103319192+kirushaz@users.noreply.github.com>
2023-10-06 12:23:52 +0500

    Create lab8_5.py

---
+++ lab8_5.py
@@ -0,0 +1,22 @@
+class sity:
+    def populathin(self):
+        pass
+
+class ekb(sity):
+    def populathin(self):
+        print('Население 1 300 000')
+
+class perm(sity):
+    def populathin(self):
+        print('Население 1 100 000')
+
+ekb = ekb()
+perm = perm()
+
+ekb.populathin()
+perm.populathin()
+
+
+
+
+
