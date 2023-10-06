2ee5fdae46219a7bd098beca863f5766d1823f75
kirushaz <103319192+kirushaz@users.noreply.github.com>
2023-10-06 12:10:44 +0500

    Create lab8_1.py

---
+++ lab8_1.py
@@ -0,0 +1,9 @@
+class sity:
+    def __init__(self, name,population):
+        self.name = name
+        self.population = population
+
+sity = sity('ekb', 200000000)
+
+print(sity.name , sity.population)
+
